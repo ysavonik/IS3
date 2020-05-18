f8d46c62a0b2e6cd4ad0f67e8ffe225ae1d8f56c
Savonik Yaroslav <svitsavonik@gmail.com>
2020-05-18 20:33:39 +0300

    A* alg

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ from random import randint, choice
 import subprocess
 import platform
 import time
+import numpy as np
 
 HEIGHT = 20
 WIDTH = 20
@@ -53,7 +54,7 @@ def draw_grid(g, width=2):
         print()
 
 
-def get_walls(g: MapGrid, pct=.25) -> list:
+def get_walls(g: MapGrid, pct=.70) -> list:
         out = []
         for i in range(int(g.height*g.width*pct)//2):
 
@@ -136,22 +137,136 @@ def dijsktra(graph, initial, end):
         current_node = next_node
     # Reverse path
     path = path[::-1]
-    print('iterations: ', iterations)
+    print('iterations Dejk: ', iterations)
     return path
+
+
+class Node():
+    """A node class for A* Pathfinding"""
+
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __eq__(self, other):
+        return self.position == other.position
+
+
+def astar(maze, start, end):
+    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
+    iterations = 0
+    # Create start and end node
+    start_node = Node(None, start)
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = Node(None, end)
+    end_node.g = end_node.h = end_node.f = 0
+
+    # Initialize both open and closed list
+    open_list = []
+    closed_list = []
+
+    # Add the start node
+    open_list.append(start_node)
+
+    # Loop until you find the end
+    while len(open_list) > 0:
+        iterations = iterations + 1
+        # Get the current node
+        current_node = open_list[0]
+        current_index = 0
+        for index, item in enumerate(open_list):
+            iterations = iterations + 1
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
+
+        # Pop current off open list, add to closed list
+        open_list.pop(current_index)
+        closed_list.append(current_node)
+
+        # Found the goal
+        if current_node == end_node:
+            path = []
+            current = current_node
+            while current is not None:
+                iterations = iterations + 1
+                path.append(current.position)
+                current = current.parent
+            print('iterations A*: ', iterations)
+            return path[::-1] # Return reversed path
+
+        # Generate children
+        children = []
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
+            iterations = iterations + 1
+
+            # Get node position
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+            # Make sure within range
+            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
+                continue
+
+            # Make sure walkable terrain
+            if maze[node_position[0]][node_position[1]] != 0:
+                continue
+
+            # Create new node
+            new_node = Node(current_node, node_position)
+
+            # Append
+            children.append(new_node)
+
+        # Loop through children
+        for child in children:
+            iterations = iterations + 1
+
+            # Child is on the closed list
+            for closed_child in closed_list:
+                if child == closed_child:
+                    continue
+
+            # Create the f, g, and h values
+            child.g = current_node.g + 1
+            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+            child.f = child.g + child.h
+
+            # Child is already in the open list
+            for open_node in open_list:
+
+                iterations = iterations + 1
+                if child == open_node and child.g > open_node.g:
+                    continue
+
+            # Add the child to the open list
+            open_list.append(child)
+
 
 def main():
     g = MapGrid(WIDTH, HEIGHT, (randint(0, HEIGHT-1), randint(0, WIDTH-1)), (randint(0, HEIGHT-1), randint(0, WIDTH-1)))
     g.walls = get_walls(g)
     draw_grid(g)
     graph = create_graph(g)
-    # print(g.start)
-    # print(g.goal)
-    # for i in graph:
-    #     print(i, ':', graph[i])
-    # print('*****\n')
     path = dijsktra(graph, g.start, g.goal)
     path.pop(0)
-    print(path)
+    print("path_dej: ", path)
+    maze = [[0 for x in range(g.width)] for y in range(g.height)]
+
+    for wall in g.walls:
+        maze[wall[0]][wall[1]] = 1
+
+    start = g.start
+    end = g.goal
+
+    path_a = astar(maze, start, end)
+    path_a.pop(0)
+    print("path_a: ", path_a)
+
+
     moves = 0
     input()
     while g.player != g.goal and path:
@@ -173,7 +288,7 @@ def main():
         clear()
 
     print("You made it!")
-    print('moves:', moves)
+    # print('moves:', moves)
 
 
 if __name__ == '__main__':
